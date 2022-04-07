72f37ca5caf8bb8984d753779c9ced415313f170
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-04-06 21:27:15 -0300

    adicionando classe de invalido se for invalido o valor do campo

--- js/validacao.js
+++ js/validacao.js
@@ -4,6 +4,12 @@
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalido");
+    } else {
+        input.parentElement.classList.add("input-container--invalido");
+    }
 }
 
 const validadores = {
